0ddc411eab0d731d38f5b6d8cd350b0c2cf5d631
garvitbandil <softwarebug@users.noreply.github.com>
2021-02-03 10:22:49 +0530

    New Sort

---
+++ src/NewSort.java
@@ -0,0 +1,28 @@
+
+public class NewSort {
+
+	
+		   public static void main(String args[]){
+		      int array[] = {12,54,76,14,65,75,79};
+		      int size = array.length;
+
+		      for (int i = 0 ;i< size-1; i++){
+		         int min = i;
+
+		         for (int j = i+1; j<size; j++){
+		            if (array[j] < array[min]){
+		            min = j;
+		            }
+		         }
+		         int temp = array[min];
+		         array[min] = array[i];
+		         array[i] = temp;
+		      }
+
+		      for (int i = 0 ;i< size; i++){
+		         System.out.print(" "+array[i]);
+		      }
+		   }  
+		}
+
+
--- src/bubbleSort.java
+++ src/bubbleSort.java
@@ -1,4 +1,4 @@
-public class BubbleSort {
+public class bubbleSort {
    static void bubbleSort(int[] arr) {
       int n = arr.length;
       int temp = 0;
